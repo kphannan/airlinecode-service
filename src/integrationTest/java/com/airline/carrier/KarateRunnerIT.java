c45e040f67387ed0f1e07086f834a3071d93f497
Kevin Hannan <kphannan@gmail.com>
2022-02-03 09:22:07 -0500

    Rearranged feature files to a feature directory

--- src/integrationTest/java/com/airline/carrier/KarateRunnerIT.java
+++ src/integrationTest/java/com/airline/carrier/KarateRunnerIT.java
@@ -5,15 +5,24 @@ import com.intuit.karate.junit5.Karate;
 // import lombok.extern.log4j.Log4j2;
 
 
+/**
+ * Main BDD test runner.
+ */
 // @Log4j2
-public class KarateRunnerIT {
-
+public class KarateRunnerIT
+{
+    /**
+     * Executes all feature files found in this package and below.
+     *
+     * @return
+     */
     @Karate.Test
-    public Karate testAll() {
-        // log.info( "Run Karate Integration Tests" );
-        System.out.println( "Run Karate Integration Tests" );
+    public Karate testAll()
+    {
+        // log.info( "Run Karate BDD Tests" );
+        // System.out.println( "Run Karate BDD Tests" );
 
-        return new Karate().relativeTo(getClass());
+        return new Karate().relativeTo( getClass() );
     }
 
 }
